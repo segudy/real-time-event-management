aa7bfc4ea9af958884ba293ab23dabad99d75279
segudy <businessegudy@gmail.com>
2025-06-22 17:56:48 +0200

    feat(auth): implement full register feature with frontend backend placeholders

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@
 import { Routes } from '@angular/router';
 import { EventListComponent } from './event-list/event-list';
 import { LoginComponent } from './login/login';
+import { RegisterComponent } from './register/register'; 
 
 export const routes: Routes = [
   // Route für die Event-Liste
@@ -10,6 +11,10 @@ export const routes: Routes = [
   // Route für unsere Login-Seite
   { path: 'login', component: LoginComponent },
 
+  // Route für unsere Registrierungs-Seite
+  { path: 'register', component: RegisterComponent },
+
   // Standard-Route (optional, leitet auf /events um)
   { path: '', redirectTo: '/events', pathMatch: 'full' },
+
 ];
